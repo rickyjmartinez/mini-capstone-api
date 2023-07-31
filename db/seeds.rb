84809eeb535057eefe5fb1bba3a2f787501320e5
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-31 15:14:25 -0400

    add 3 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "starter set", price: 20, image_url: "https://i5.walmartimages.com/seo/Dungeons-Dragons-Starter-Set-Book-Supplement_d48f4511-173f-4d3f-9f79-a7636dbf3d8f_1.64ad6f13280fcc303930bcf152118346.jpeg", description: "a kit to get started in dnd.")
+product.save
