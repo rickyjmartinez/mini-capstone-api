29823c63bbf249fb86d1de59fca821bab2c38e83
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-04 12:43:20 -0400

    add inventory column and changed price data to add decimals

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_162519) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_163449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 6, scale: 2
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "inventory"
   end
 
 end
