31f6eb028d53dde956761d72f7356af08854cffd
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-09 13:55:36 -0400

    add authentication to order create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,29 +5,24 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(
-      user_id: params[:user_id],
-      product_id: params[:product_id],
-      quantity: params[:product_id],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
-
-      # name: params[:name],
-      # price: params[:price],
-      # description: params[:description],
-      # supplier_id: params[:supplier_id],
-    )
-
-    if @order.save
+    if current_user
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:product_id],
+        subtotal: params[:subtotal],
+        tax: params[:tax],
+        total: params[:total],
+      )
+      @order.save
       render :show
     else
-      render json: { error: @order.errors.full_messages }, status: 422
+      render json: { message: "please log in to place an order" }, status: 422
     end
   end
 
   def show
-    @order = Order.find_by(id: params["id"])
+    @order = Order.find_by(id: params[:id])
     render :show
   end
 end
