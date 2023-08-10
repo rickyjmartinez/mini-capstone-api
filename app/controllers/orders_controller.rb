04b8c5465692814f96e9880564780dd5d20db1e5
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-10 12:22:01 -0400

    add before_action :authenticate_user to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
     # orders = Order.where(user_id: current_user.id)
     @orders = current_user.orders
